3f5be3cfc02ac7ad54429cad1d8ff6da832101e2
Rohan Yadav <rohany@alumni.cmu.edu>
2022-12-16 20:50:07 -0800

    src/sparse/array/csc: attempt to fix CI compilation errors (#84)
    Signed-off-by: Rohan Yadav <rohany@alumni.cmu.edu>
    
    Signed-off-by: Rohan Yadav <rohany@alumni.cmu.edu>

--- src/sparse/array/csc/spmv.cu
+++ src/sparse/array/csc/spmv.cu
@@ -78,7 +78,7 @@ struct CSCSpMVColSplitImpl<VariantKind::GPU> {
                                            &beta,
                                            cusparse_y,
                                            cusparseDataType<VAL_TY>(),
-#if (CUSPARSE_VER_MAJOR < 11 || CUSPARSE_VER_MINOR < 2)
+#if (CUSPARSE_VER_MAJOR < 11 || (CUSPARSE_VER_MAJOR == 11 && CUSPARSE_VER_MINOR < 2))
                                            CUSPARSE_MV_ALG_DEFAULT,
 #else
                                            CUSPARSE_SPMV_ALG_DEFAULT,
@@ -99,7 +99,7 @@ struct CSCSpMVColSplitImpl<VariantKind::GPU> {
                                 &beta,
                                 cusparse_y,
                                 cusparseDataType<VAL_TY>(),
-#if (CUSPARSE_VER_MAJOR < 11 || CUSPARSE_VER_MINOR < 2)
+#if (CUSPARSE_VER_MAJOR < 11 || (CUSPARSE_VER_MAJOR == 11 && CUSPARSE_VER_MINOR < 2))
                                 CUSPARSE_MV_ALG_DEFAULT,
 #else
                                 CUSPARSE_SPMV_ALG_DEFAULT,
