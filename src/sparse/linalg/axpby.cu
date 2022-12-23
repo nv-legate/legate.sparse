9d538588e0d48f8c882ec0fa8de4246b029b793b
Rohan Yadav <rohany@alumni.cmu.edu>
2022-12-23 10:59:02 -0800

    src/sparse/linalg: compilation update
    Signed-off-by: Rohan Yadav <rohany@alumni.cmu.edu>

--- src/sparse/linalg/axpby.cu
+++ src/sparse/linalg/axpby.cu
@@ -29,7 +29,7 @@ __global__ void axpby_kernel(size_t elems,
                              AccessorRW<VAL_TY, 1> y,
                              AccessorRO<VAL_TY, 1> x,
                              AccessorRO<VAL_TY, 1> a,
-                             AccessorRO<VAL_TY, 1> a)
+                             AccessorRO<VAL_TY, 1> b)
 {
   const auto idx = global_tid_1d();
   if (idx >= elems) return;
