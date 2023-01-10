e2d9e1df9ca6d6f723d6966b0b1698341872b9a5
Rohan Yadav <rohany@alumni.cmu.edu>
2023-01-10 11:03:34 -0800

    scripts/summit: reformat and add iteration control to dot scripts
    Signed-off-by: Rohan Yadav <rohany@alumni.cmu.edu>

--- scripts/summit/run_petsc_dot_microbenchmark.sh
+++ scripts/summit/run_petsc_dot_microbenchmark.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 if [[ ! -n $PETSC_DIR ]]; then
-	echo "The PETSC_DIR variable must be set."
-	exit 1
+    echo "The PETSC_DIR variable must be set."
+    exit 1
 fi
 
+EXP_ITERS=${EXP_ITERS:-1}
 ITERS=200
 SIZE=10000000
 COMMON_ARGS="-i $ITERS -nnz-per-row 11"
@@ -15,19 +16,25 @@ weak_scale() {
 
 # CPU runs.
 if [[ -n $CPU_SOCKETS ]]; then
-	for sockets in $CPU_SOCKETS; do
-		cmd="jsrun -n $(($sockets * 20)) -c 1 -b rs $PETSC_DIR/dot_microbenchmark -n $(weak_scale $sockets) $COMMON_ARGS $ARGS"
-		echo $cmd
-		eval $cmd
-	done
+    for sockets in $CPU_SOCKETS; do
+        cmd="jsrun -n $(($sockets * 20)) -c 1 -b rs $PETSC_DIR/dot_microbenchmark -n $(weak_scale $sockets) $COMMON_ARGS $ARGS"
+        for iter in {1..$EXP_ITERS}; do
+            echo "CPU SOCKETS = $sockets:"
+            echo $cmd
+            eval $cmd
+        done
+    done
 fi
 
 # GPU runs.
 if [[ -n $GPUS ]]; then
-	GPU_ARGS="$COMMON_ARGS -vec_type cuda -mat_type aijcusparse -gpu"
-	for gpus in $GPUS; do
-		cmd="jsrun -n $gpus -g 1 -c 4 -b rs --smpiargs=\"-gpu\" $PETSC_DIR/dot_microbenchmark $GPU_ARGS -n $(weak_scale $gpus) $ARGS"
-		echo $cmd
-		eval $cmd
-	done
+    GPU_ARGS="$COMMON_ARGS -vec_type cuda -mat_type aijcusparse -gpu"
+    for gpus in $GPUS; do
+        cmd="jsrun -n $gpus -g 1 -c 4 -b rs --smpiargs=\"-gpu\" $PETSC_DIR/dot_microbenchmark $GPU_ARGS -n $(weak_scale $gpus) $ARGS"
+        for iter in {1..$EXP_ITERS}; do
+            echo "GPUS = $gpus:"
+            echo $cmd
+            eval $cmd
+        done
+    done
 fi
